52d639aca120c20fbdd5d307d2f865264db91fcf
YJE888 <75539276+YJE888@users.noreply.github.com>
2023-02-05 17:05:38 +0900

    Update copy-img-delete.sh

--- 4.Cloud-Function/harbor/copy-img-delete.sh
+++ 4.Cloud-Function/harbor/copy-img-delete.sh
@@ -18,12 +18,14 @@ HOST=$(awk '/^HOST/{print $3}' ${CONFIG_FILE})
 AUTH=$(awk '/^AUTH/{print $3}' ${CONFIG_FILE})
 TOKEN=$(awk '/^TOKEN/{print $3}' ${CONFIG_FILE})
 
+# 이미지 삭제
 curl -X DELETE \
   "https://${HOST}/api/v2.0/projects/share/repositories/${IMG_NAME}/artifacts/${TAG}" \
   -H "accept: application/json" \
   -H "authorization: Basic ${AUTH}" \
   -H "X-Harbor-CSRF-Token: ${TOKEN}"
 
+# 이미지 존재 유무 
 curl -X GET \
   "https://${HOST}/api/v2.0/projects/share/repositories/${IMG_NAME}/artifacts/${TAG}/tags?page=1&page_size=10&with_signature=false&with_immutable_status=false" \
   -H "accept: application/json" \
